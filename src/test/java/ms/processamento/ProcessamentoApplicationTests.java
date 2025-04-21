1623ca49bcd28506e67ca7e88ee58bdd75bd546a
bispobr <156684382+bispobr@users.noreply.github.com>
2025-04-21 10:27:48 -0300

    feat: adiciona config e estrutura inicial

---
+++ src/test/java/ms/processamento/ProcessamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package ms.processamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcessamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
