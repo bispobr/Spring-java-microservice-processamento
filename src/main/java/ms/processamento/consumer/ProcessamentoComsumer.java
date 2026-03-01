6e6ab00d883435f8b840a7fb60285dadd7a05d42
bispobr <156684382+bispobr@users.noreply.github.com>
2026-03-01 11:23:51 -0300

    feat: add logs

--- src/main/java/ms/processamento/consumer/ProcessamentoComsumer.java
+++ src/main/java/ms/processamento/consumer/ProcessamentoComsumer.java
@@ -6,13 +6,13 @@ import ms.processamento.dto.PedidoDTO;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
-
+@Slf4j
 @Component
 public class ProcessamentoComsumer {
 
     @RabbitListener(queues = "${broker.queue.processamento.name}")
     public void listenerProcessamentoQueue(@Valid PedidoDTO pedidoDTO){
-
+        log.info("Comsumindo fila");
         System.out.println(pedidoDTO.descricao());
     }
 }
